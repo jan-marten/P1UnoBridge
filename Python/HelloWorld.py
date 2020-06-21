5867f14ddf24f64fa40c2b08e806a3ec4f5aa56c
Jan Marten <janmarten@regeling.com>
2020-06-21 21:13:35 +0200

    Added HelloWorld.py Python script

---
+++ Python/HelloWorld.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.7
+
+# R-pi; locate the correct Python version using locate python
+# Use only lf (\n) as line-endings, no crlf allowed
+# After upload, CHMOD to a minimum of 744 (allow execution for user)
+
+print("Hello world!")
